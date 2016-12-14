56196d2dec6056a28f31f0df63b25f5df2b8e1eb
Guénaëlle Deschamps <guenaelle.deschamps@gmail.com>
2016-12-14 16:10:11 +0100

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
  ### application.bat - 2016 - Guénaëlle Deschamps ###
-echo "Copyright Guénaëlle Deschamps - 2016"
-echo "Build: 10:00"
-echo "version 1.1"
+version="2"
+build_hour="11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
